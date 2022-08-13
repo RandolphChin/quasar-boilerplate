30aa234fbea48a54448221ce53451a4ef8b451b8
Gogs <gogs@fake.local>
2022-08-13 22:43:52 +0800

    add login layout

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page>
+      <q-page-container>
+        <router-view/>
+      </q-page-container>
+    </q-page>
+  </q-layout>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'LoginLayout'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,11 +4,19 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {path: '', component: () => import('pages/IndexPage.vue')},
-      {path: '/login', component: () => import('pages/Login.vue')},
-      {path: '/profile', component: () => import('pages/Profile.vue')}
+      {path: '/profile', component: () => import('pages/Profile.vue')},
+      {path: '/contact', component: () => import('pages/Contact.vue')},
+      // {path: '/login', component: () => import('pages/Login.vue')},
+      {path: '/product', component: () => import('pages/ProductCatalogues.vue')}
     ]
   },
-
+  {
+    path: '/login',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      {path: '', component: () => import('pages/Login.vue')}
+    ]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
